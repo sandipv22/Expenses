35e249e896f3f838f1e83ecbb9cdcf4a0e0076f0
Sandip <sandipvaghela311@gmail.com>
2019-02-17 18:07:26 +0530

    Replaced Save action with FAB

--- app/src/main/java/com/afterroot/expenses/fragment/NewGroupFragment.kt
+++ app/src/main/java/com/afterroot/expenses/fragment/NewGroupFragment.kt
@@ -2,7 +2,9 @@ package com.afterroot.expenses.fragment
 
 import android.os.Bundle
 import android.util.Log
-import android.view.*
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import android.widget.MultiAutoCompleteTextView
 import androidx.fragment.app.Fragment
 import com.afterroot.expenses.R
@@ -10,9 +12,11 @@ import com.afterroot.expenses.model.Group
 import com.afterroot.expenses.utils.DBConstants
 import com.afterroot.expenses.utils.FirebaseUtils
 import com.afterroot.expenses.utils.Utils
+import com.afterroot.expenses.utils.getDrawableExt
 import com.android.ex.chips.BaseRecipientAdapter
 import com.google.firebase.firestore.FirebaseFirestore
 import kotlinx.android.synthetic.main.activity_home.*
+import kotlinx.android.synthetic.main.content_home.*
 import kotlinx.android.synthetic.main.fragment_new_group.*
 import org.jetbrains.anko.design.snackbar
 import java.util.*
@@ -20,7 +24,7 @@ import java.util.*
 class NewGroupFragment : Fragment() {
 
     private val db = FirebaseFirestore.getInstance()
-    private val TAG = "NewGroupFragment"
+    private val _tag = "NewGroupFragment"
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.fragment_new_group, container, false)
@@ -28,31 +32,24 @@ class NewGroupFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        setHasOptionsMenu(true)
         text_input_group_members.apply {
             setTokenizer(MultiAutoCompleteTextView.CommaTokenizer())
         }
         val recAdapter = BaseRecipientAdapter(BaseRecipientAdapter.QUERY_TYPE_PHONE, activity!!)
         recAdapter.isShowMobileOnly = false
         text_input_group_members.setAdapter(recAdapter)
-    }
 
-    private var saveItem: MenuItem? = null
-    override fun onCreateOptionsMenu(menu: Menu?, inflater: MenuInflater?) {
-        super.onCreateOptionsMenu(menu, inflater)
-        saveItem = menu!!.add("Create")
-        with(saveItem!!) {
-            setIcon(R.drawable.ic_done)
-            setShowAsAction(MenuItem.SHOW_AS_ACTION_ALWAYS)
-            setOnMenuItemClickListener {
+        activity!!.fab.apply {
+            setImageDrawable(activity!!.getDrawableExt(R.drawable.ic_done))
+            setOnClickListener {
                 val selected = text_input_group_members.sortedRecipients
                 if (selected.isNotEmpty()) {
                     val userMAp = HashMap<String?, Int>()
                     progress.visibility = View.VISIBLE
-                    Log.d(TAG, "onActivityResult: Adding contacts to Map")
+                    Log.d(_tag, "onActivityResult: Adding contacts to Map")
                     var size = selected.size
                     for (item in selected) {
-                        Log.d(TAG, "onActivityResult: Query Started for ${item.entry.destination}")
+                        Log.d(_tag, "onActivityResult: Query Started for ${item.entry.destination}")
                         val value = Utils.formatPhone(activity!!, item.entry.destination)
 
                         db.collection(DBConstants.USERS)
@@ -61,13 +58,13 @@ class NewGroupFragment : Fragment() {
                                 .addOnSuccessListener { querySnapshot ->
                                     if (querySnapshot.documents.isNotEmpty()) {
                                         val id = querySnapshot.documents[0].id
-                                        Log.d(TAG, "getUID: Query Successful for $value, with id $id")
+                                        Log.d(_tag, "getUID: Query Successful for $value, with id $id")
                                         userMAp[id] = DBConstants.TYPE_MEMBER
                                         size--
                                         if (size == 0) {
                                             userMAp[FirebaseUtils.firebaseUser!!.uid] = DBConstants.TYPE_ADMIN
                                             progress.visibility = View.VISIBLE
-                                            Log.d(TAG, "onCreateOptionsMenu: Creating Group")
+                                            Log.d(_tag, "onCreateOptionsMenu: Creating Group")
                                             val group = Group(input_group_name.text.toString(), Date(), userMAp)
                                             db.collection(DBConstants.GROUPS).add(group).addOnSuccessListener {
                                                 activity!!.root_layout.snackbar("Group Created")
@@ -79,17 +76,14 @@ class NewGroupFragment : Fragment() {
                                             }
                                         }
                                     } else {
-                                        Log.d(TAG, "getUID: User Not Available")
+                                        Log.d(_tag, "getUID: User Not Available")
                                         activity!!.root_layout.snackbar("${item.entry.destination} is not available")
                                         progress.visibility = View.INVISIBLE
                                     }
-                                }.addOnFailureListener { Log.d(TAG, "getUID: Query Failed") }
+                                }.addOnFailureListener { Log.d(_tag, "getUID: Query Failed") }
                     }
                 }
-                return@setOnMenuItemClickListener true
             }
         }
     }
-
-
 }
