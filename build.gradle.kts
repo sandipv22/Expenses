5f07a39138ebb4c0d3749881cc2358563df490a7
Sandip <sandipvaghela311@gmail.com>
2019-02-25 11:37:40 +0530

    Changed language to Kotlin Script

--- build.gradle.kts
+++ build.gradle.kts
@@ -17,16 +17,15 @@
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 
 buildscript {
-    ext.kotlin_version = '1.3.21'
     repositories {
         google()
         jcenter()
     }
     dependencies {
-        classpath 'com.android.tools.build:gradle:3.3.1'
-        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
-        classpath 'com.google.gms:google-services:4.2.0'
-        classpath "android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0-beta02"
+        classpath("com.android.tools.build:gradle:3.3.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.21")
+        classpath("com.google.gms:google-services:4.2.0")
+        classpath("android.arch.navigation:navigation-safe-args-gradle-plugin:1.0.0-beta02")
     }
 }
 
@@ -34,11 +33,11 @@ allprojects {
     repositories {
         google()
         jcenter()
-        maven { url 'https://jitpack.io' }
+        maven { url = uri("https://jitpack.io") }
         mavenCentral()
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
