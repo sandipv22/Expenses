bba5bf4de4aa903e56847e1416913ae9b495d576
Sandip <sandipvaghela311@gmail.com>
2019-03-01 22:57:06 +0530

    Migrate to ViewModel

--- app/src/main/java/com/afterroot/expenses/fragment/ExpenseListFragment.kt
+++ app/src/main/java/com/afterroot/expenses/fragment/ExpenseListFragment.kt
@@ -22,33 +22,34 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
-import androidx.appcompat.widget.AppCompatTextView
 import androidx.fragment.app.Fragment
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.afterroot.expenses.R
+import com.afterroot.expenses.adapter.ExpenseAdapter
+import com.afterroot.expenses.model.Expense.Companion.TYPE_EXPENSE
 import com.afterroot.expenses.model.ExpenseItem
-import com.afterroot.expenses.utils.*
-import com.firebase.ui.firestore.FirestoreRecyclerAdapter
-import com.firebase.ui.firestore.FirestoreRecyclerOptions
+import com.afterroot.expenses.model.ExpensesViewModel
+import com.afterroot.expenses.utils.Constants
+import com.afterroot.expenses.utils.ListClickCallbacks
+import com.afterroot.expenses.utils.visible
 import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.firestore.FirebaseFirestoreException
 import com.google.firebase.firestore.FirebaseFirestoreSettings
-import com.google.firebase.firestore.Query
+import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.content_home.*
 import kotlinx.android.synthetic.main.fragment_expense_list.*
-import kotlinx.android.synthetic.main.list_item_expense.view.*
 
-class ExpenseListFragment : Fragment() {
-    private var firestoreAdapter: FirestoreRecyclerAdapter<ExpenseItem, ExpenseViewHolder>? = null
+class ExpenseListFragment : Fragment(), ListClickCallbacks<QuerySnapshot> {
+    private var adapter: ExpenseAdapter? = null
     private var db: FirebaseFirestore? = null
     lateinit var groupDocID: String
-    val _tag = "ExpenseListFragment"
-    val args: ExpenseListFragmentArgs by navArgs()
+    private val _tag = "ExpenseListFragment"
+    private val args: ExpenseListFragmentArgs by navArgs()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
@@ -66,7 +67,6 @@ class ExpenseListFragment : Fragment() {
         }, 200)
 
         activity!!.fab.apply {
-            setImageDrawable(activity!!.getDrawableExt(R.drawable.ic_add, R.color.icon_fill))
             setOnClickListener {
                 Log.d(_tag, "onViewCreated: FAB Clicked")
                 val action = ExpenseListFragmentDirections.toAddExpense(groupDocID)
@@ -76,73 +76,32 @@ class ExpenseListFragment : Fragment() {
     }
 
     private fun initFirebaseDb() {
-        val query = db!!.collection(DBConstants.GROUPS)
-                .document(groupDocID)
-                .collection(DBConstants.EXPENSES).orderBy("date", Query.Direction.DESCENDING)
-
         Log.d(_tag, "initFirebaseDb: groupId: $groupDocID")
 
-        val options = FirestoreRecyclerOptions.Builder<ExpenseItem>()
-                .setQuery(query, ExpenseItem::class.java)
-                .setLifecycleOwner(this)
-                .build()
-
-        firestoreAdapter = object : FirestoreRecyclerAdapter<ExpenseItem, ExpenseViewHolder>(options) {
-            override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ExpenseViewHolder {
-                val holderView = LayoutInflater.from(parent.context).inflate(R.layout.list_item_expense, parent, false)
-                return ExpenseViewHolder(holderView)
-            }
-
-            override fun onBindViewHolder(holder: ExpenseViewHolder, position: Int, model: ExpenseItem) {
-                holder.amountText.text = String.format("%s%d",
-                        holder.amountText.context.resources.getString(R.string.rs_symbol), model.amount)
-                holder.noteText.text = String.format("%s, with %s", model.note, Utils.formatNames(model.with!!))
-                holder.categoryText.text = model.category
-
-                with(holder.itemView) {
-                    tag = model
-                    val id = snapshots.getSnapshot(holder.adapterPosition).id
-                    setOnClickListener {
-                        val bundle = Bundle().apply {
-                            putSerializable(Constants.KEY_EXPENSE_SERIALIZE, tag as ExpenseItem)
-                        }
-                        view!!.findNavController().navigate(R.id.toExpenseDetail, bundle)
-                        //listener!!.onListItemClick(tag as ExpenseItem, id)
-                    }
-
-                    setOnLongClickListener {
-                        //listener!!.onListItemLongClick(tag as ExpenseItem, id)
-                        return@setOnLongClickListener true
-                    }
-                }
-            }
-
-            override fun onDataChanged() {
-                Log.d(_tag, "onDataChanged: ItemCount: $itemCount")
-                if (itemCount == 0) {
-                    activity!!.text_no_expenses.visible(true)
-                } else {
-                    activity!!.text_no_expenses.visible(false)
-                }
-            }
-
-            override fun onError(e: FirebaseFirestoreException) {
-                Toast.makeText(activity!!, e.message.toString(), Toast.LENGTH_SHORT).show()
-            }
-        }
+        adapter = ExpenseAdapter(this)
 
         list.apply {
             val lm = LinearLayoutManager(this.context)
             layoutManager = lm
             addItemDecoration(DividerItemDecoration(this.context, lm.orientation))
-            adapter = firestoreAdapter
         }
-        activity!!.progress.visible(false)
+
+        val expensesViewModel = ViewModelProviders.of(this).get(ExpensesViewModel::class.java)
+        expensesViewModel.getSnapshot(groupDocID).observe(this, Observer<QuerySnapshot> { snapshot ->
+            adapter!!.setSnapshot(snapshot, TYPE_EXPENSE)
+            list.adapter = adapter
+            activity!!.progress.visible(false)
+        })
     }
 
-    inner class ExpenseViewHolder(view: View) : androidx.recyclerview.widget.RecyclerView.ViewHolder(view) {
-        val amountText: AppCompatTextView = view.item_amount
-        val noteText: AppCompatTextView = view.item_note
-        val categoryText: AppCompatTextView = view.item_category
+    override fun onListItemClick(item: QuerySnapshot?, docId: String, position: Int) {
+        val bundle = Bundle().apply {
+            putSerializable(Constants.KEY_EXPENSE_SERIALIZE, adapter!!.mList[position] as ExpenseItem)
+        }
+        Log.d(_tag, "onListItemClick: ${bundle.getSerializable(Constants.KEY_EXPENSE_SERIALIZE)}")
+        view!!.findNavController().navigate(R.id.toExpenseDetail, bundle)
+    }
+
+    override fun onListItemLongClick(item: QuerySnapshot?, docId: String, position: Int) {
     }
 }
