3a90d77666375837f87383fc8f9353555597cb5e
Sandip <sandipvaghela311@gmail.com>
2019-04-06 22:13:52 +0530

    Added Summary Card

--- app/src/main/java/com/afterroot/expenses/fragment/ExpenseListFragment.kt
+++ app/src/main/java/com/afterroot/expenses/fragment/ExpenseListFragment.kt
@@ -19,10 +19,14 @@ package com.afterroot.expenses.fragment
 import android.animation.LayoutTransition
 import android.os.Bundle
 import android.os.Handler
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
 import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.widget.LinearLayoutCompat
 import androidx.core.os.bundleOf
 import androidx.core.view.ViewCompat
 import androidx.fragment.app.Fragment
@@ -33,6 +37,7 @@ import androidx.navigation.fragment.FragmentNavigatorExtras
 import androidx.navigation.fragment.navArgs
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.transition.AutoTransition
 import com.afterroot.expenses.Constants
 import com.afterroot.expenses.R
 import com.afterroot.expenses.adapter.ExpenseAdapterDelegate
@@ -40,11 +45,14 @@ import com.afterroot.expenses.adapter.callback.ItemSelectedCallback
 import com.afterroot.expenses.database.DBConstants
 import com.afterroot.expenses.database.Database
 import com.afterroot.expenses.firebase.DeleteListener
+import com.afterroot.expenses.firebase.QueryCallback
 import com.afterroot.expenses.model.ExpenseItem
+import com.afterroot.expenses.model.User
 import com.afterroot.expenses.viewmodel.ExpensesViewModel
 import com.afterroot.expenses.viewmodel.ViewModelState
 import com.afterroot.expenses.visible
 import com.google.android.material.bottomsheet.BottomSheetDialog
+import com.google.firebase.firestore.DocumentSnapshot
 import com.google.firebase.firestore.QuerySnapshot
 import kotlinx.android.synthetic.main.activity_home.*
 import kotlinx.android.synthetic.main.content_home.*
@@ -73,12 +81,15 @@ class ExpenseListFragment : Fragment(), ItemSelectedCallback {
             initFirebaseDb()
         }, 50)
 
+        var height = 0
         activity!!.fab.apply {
             setOnClickListener {
                 val action = ExpenseListFragmentDirections.toAddExpense(groupDocID, null)
                 view.findNavController().navigate(action)
             }
+            height = getHeight()
         }
+        space_view.layoutParams = LinearLayoutCompat.LayoutParams(LinearLayoutCompat.LayoutParams.MATCH_PARENT, height + 32)
     }
 
     override fun onResume() {
@@ -117,6 +128,96 @@ class ExpenseListFragment : Fragment(), ItemSelectedCallback {
                 }
             }
         })
+
+
+        text_view_summary.setOnClickListener {
+            users_spinner.visible(true, AutoTransition())
+            total_spend.visible(true, AutoTransition())
+            text_view_summary.visible(false, AutoTransition())
+            expensesViewModel.getGroupMembers(groupDocID, object : QueryCallback<HashMap<String, User>> {
+                override fun onSuccess(value: HashMap<String, User>) {
+                    try {
+                        val array = ArrayList<String>()
+                        val uidMap = HashMap<Int, User>()
+                        var i = 0
+                        value.forEach {
+                            array.add(it.value.name)
+                            uidMap[i] = it.value
+                            i++
+                        }
+                        val adapter = ArrayAdapter<String>(context!!, android.R.layout.simple_spinner_item, array)
+                        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                        users_spinner.apply {
+                            this.adapter = adapter
+                            onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                                override fun onNothingSelected(parent: AdapterView<*>?) {
+                                    expensesViewModel.getSnapshot(groupDocID).observe(this@ExpenseListFragment, Observer<ViewModelState> { state ->
+                                        when (state) {
+                                            is ViewModelState.Loading -> {
+                                                activity!!.progress.visible(true)
+                                            }
+
+                                            is ViewModelState.Loaded<*> -> {
+                                                mSnapshot = state.data as QuerySnapshot
+                                                expenseAdapter!!.add(mSnapshot!!.toObjects(ExpenseItem::class.java) as List<ExpenseItem>)
+                                                activity?.apply {
+                                                    progress?.visible(false)
+                                                    text_no_expenses.visible(mSnapshot!!.documents.size == 0)
+                                                }
+                                            }
+                                        }
+                                    })
+                                }
+
+                                override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                                    try {
+                                        val uid: String? = uidMap[position]!!.uid
+                                        Log.d(_tag, "onItemSelected: $uid")
+                                        if (uid != null) {
+                                            val paidByQuery = Database.getInstance()
+                                                    .collection(DBConstants.GROUPS)
+                                                    .document(groupDocID)
+                                                    .collection(DBConstants.EXPENSES)
+                                                    .whereEqualTo(DBConstants.FIELD_PAID_BY, uid)
+                                            expensesViewModel.getSnapshot(paidByQuery).observe(this@ExpenseListFragment, Observer {
+                                                when (it) {
+                                                    is ViewModelState.Loaded<*> -> {
+                                                        val snapshot = it.data as QuerySnapshot
+                                                        val expenses = snapshot.toObjects(ExpenseItem::class.java) as List<ExpenseItem>
+                                                        //expenseAdapter!!.add(expenses) to apply filter to whole list
+                                                        var total: Long = 0
+                                                        expenses.forEach { item ->
+                                                            total += item.amount
+                                                        }
+                                                        activity!!.total_spend.text = String.format("%s%d", view!!.context.resources.getString(R.string.rs_symbol), total)
+                                                    }
+                                                    is ViewModelState.Loading -> {
+                                                    }
+                                                }
+                                            })
+                                        }
+                                    } catch (e: Exception) {
+
+                                    }
+
+                                }
+
+                            }
+                        }
+                    } catch (e: Exception) {
+
+                    }
+                }
+
+                override fun onFailed(message: String) {
+                }
+
+                override fun onSnapshot(snapshot: DocumentSnapshot) {
+                }
+
+            })
+
+        }
     }
 
     override fun onClick(position: Int, view: View?) {
